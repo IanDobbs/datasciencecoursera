b9bf1a70432bd7d1e4efa36274408366adc94083
IanDobbs <idobbs@yahoo.com>
2022-10-30 11:19:55 +0000

    correct sample output

--- rankhospital.R
+++ rankhospital.R
@@ -30,24 +30,23 @@ rankhospital <- function(state, outcome, num = "best") {
   dat_subset <- na.omit(dat_subset) #remove nas
   dat_subset <- dat_subset[order(dat_subset[, 47], dat_subset[, 2]),] #subset by rank then handle ties alphabetically
   dat_subset$Rank2 <- order((dat_subset[, 47]), na.last = TRUE)#re rank based
-  return(dat_subset[num, 2]) #display first row, column 2
+  if (num == "best")
+  {
+    num <- min(dat_subset[, 48])
+  }
+  else if (num == "worst")
+  {
+    num <- max(dat_subset[, 48])
+  }
+  else
+  {
+    num <- num
+  }
+     return(dat_subset[num, 2]) #display first row, column 2
 }    
-?num
+
+
 #test the function
-best("TX", "heart attack")
-best("MD", "heart attack")
-best("TX", "heart failure")
-best("MD", "pneumonia")
-best("NY", "heart attack")
-best("AL", "pneumonia")
-?order
-## Return hospital name in that state with lowest 30-day death rate
-dat <- read.csv("outcome-of-care-measures.csv", colClasses = "character") ## Read outcome data
-dat_subset <- subset(dat, State %in% "MD") #if true the subset data for state
-suppressWarnings(dat_subset[, 11] <- as.numeric(dat_subset[, 11]))#coerce the column to numeric to find minimum value
-dat_subset$Rank <- rank(dat_subset[, 11], na.last = TRUE, ties.method = "min") #use rank function against outcome var
-dat_subset <- na.omit(dat_subset) #remove nas
-dat_subset <- dat_subset[order(dat_subset[, 47], dat_subset[, 2]),] #order by rank then alphabetically
-dat_subset$Rank2 <- order((dat_subset[, 47]), na.last = TRUE)
-worst <- nrow(dat_subset)
-(dat_subset[worst, 2]) #display first row, column 2
+rankhospital("TX", "heart failure", 4)
+rankhospital("MD", "heart attack", "worst")
+rankhospital("MN", "heart attack", 5000)
